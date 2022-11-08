cb085e386b55438e253e50de9d5b4df1a44ba947
tauseef043 <Muhammadtauseef043@gmail.com>
2022-11-08 09:42:49 +0500

    8 nov 2022

---
+++ cypress/e2e/gettingStarted/Handling_alerts_popupWindow.cy.js
@@ -0,0 +1,46 @@
+/// <reference types="Cypress" />
+
+describe('My Second Test Suite', function() 
+{
+
+it('My FirstTest case',function() {
+
+
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+cy.get('#alertbtn').click()
+cy.on('window:alert',(str)=>{
+
+    expect(str).to.equal('Hello , share this practice page and share your knowledge')
+})
+cy.get('#confirmbtn').click()
+
+
+cy.on('window:confirm',(str)=>{
+
+    expect(str).to.equal('Hello , Are you sure you want to confirm?')
+})
+
+// Handling Multiple tabs
+
+// Cypress cannot handle multiple tabs so we have to amnage it on same tabs by removing target element
+
+
+cy.get('#opentab').invoke('removeAttr','target').click()
+
+
+
+
+
+
+
+
+
+
+})
+
+
+}  )
+
+
+
