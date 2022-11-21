d5e1b3cdb881a8260940a8fc2345b6cf680757b4
tauseef043 <Muhammadtauseef043@gmail.com>
2022-11-21 11:59:22 +0500

    21 nov 2022

--- cypress.config.js
+++ cypress.config.js
@@ -14,11 +14,19 @@ module.exports = defineConfig({
       URL:"https://rahulshettyacademy.com/angularpractice",
 
     },
-
+    "retries": {
+      // Configure retry attempts for `cypress run`
+      // Default is 0
+      "runMode": 2,
+      // Configure retry attempts for `cypress open`
+      // Default is 0
+      "openMode": 0
+    },
 
     
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+ 
   },
 });
